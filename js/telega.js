fc30cecbf2522299b713922491bc4b24602b85f7
Sergey Tarasenko <67330524+TatisWeb@users.noreply.github.com>
2024-10-26 18:10:30 +0400

    Add files via upload

--- js/telega.js
+++ js/telega.js
@@ -19,7 +19,7 @@ const API = `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`
 
     const { name, msg, phone } = Object.fromEntries(new FormData(formModal).entries());
     
-    const text = `Заявка с сайта www.tatisweb.ru ${name}!\nСообщение: ${msg}\nТелефон:${phone}`;
+    const text = `Заявка с сайта www.tatisweb.github.io\n Имя: ${name}!\nСообщение: ${msg}\nТелефон:${phone}`;
 
 
     try {
@@ -38,7 +38,7 @@ const API = `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`
         
         if (response.ok) {
           
-             alert('Спасибо за обращение! Мы свяжемся с вами в ближайшее время.');
+             alert('Спасибо за обращение! Я свяжусь с вами в ближайшее время.');
             formModal.reset()
         } else {
             throw new Error(response.statusText);
@@ -55,3 +55,56 @@ const API = `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`
 }
 
 });
+
+	document.addEventListener('DOMContentLoaded', function() {
+          const formModal = document.getElementById('formPrice');
+          formModal.addEventListener('submit', formSend);
+    
+    async function formSend(event) {
+      event.preventDefault();
+
+    const formModal = document.getElementById('formPrice');
+    const formBtn = document.querySelector('.form__button')
+    const formSendResult = document.querySelector('.form__send-result-modal')
+    formSendResult.textContent = '';
+
+
+    const { name, msg, phone } = Object.fromEntries(new FormData(formModal).entries());
+    
+    const text = `Заявка с сайта www.tatisweb.github.io нижняя форма\n Имя: ${name}!\nСообщение: ${msg}\nТелефон:${phone}`;
+
+
+    try {
+        formBtn.textContent = 'Отправляем...';
+
+        const response = await fetch(API, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                chat_id: TELEGRAM_CHAT_ID,
+                text,
+            })
+        })
+        
+        if (response.ok) {
+          
+             alert('Спасибо за обращение! Я свяжусь с вами в ближайшее время.');
+            formModal.reset()
+        } else {
+            throw new Error(response.statusText);
+        }
+
+    } catch (error) {
+        console.error(error);
+        formSendResult.textContent = 'Заявка не отправлена! Попробуйте позже.';
+        formSendResult.style.color = 'red';
+
+    } finally {
+        formBtn.textContent = 'Отправить';
+    }
+}
+
+});
+
